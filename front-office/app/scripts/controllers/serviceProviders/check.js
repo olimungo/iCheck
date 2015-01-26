e8efd25b6d571401ff146506ba9dd450d715a450
Olivier Mungo <olivier@mungo.eu>
2015-01-26 23:09:48 +0100

    no message

---
+++ front-office/app/scripts/controllers/serviceProviders/check.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('frontOfficeApp')
+.controller('ServiceProvidersCheckCtrl', function ($scope, $timeout, $location) {
+  $scope.clock = '';
+  $scope.tickInterval = 5000;
+
+  var tick = function() {
+      $scope.clock = Date.now() // get the current time
+      $timeout(tick, $scope.tickInterval); // reset the timer
+  }
+
+  tick();
+
+  $scope.home = function () {
+    $location.path('/serviceProviders/home');
+  };
+});
