b7bf2b1b2a262c7bd4df4f5bdbb09f5c65a27421
Olivier Mungo <olivier@mungo.eu>
2015-01-30 20:22:19 +0100

    no message

---
+++ front-end/app/scripts/controllers/serviceProviders/requests.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('frontOfficeApp')
+.controller('ServiceProvidersRequestsCtrl', function ($scope) {
+  $scope.requests = [
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+    { date: '01/01/2015', period: 'Morning in', time:'7:33', comment: 'Went directly to a meeting' },
+  ];
+});
